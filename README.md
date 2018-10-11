6929a903769bc933a5477dd6d877d1f3732d30a1
jeromeklam <jeromeklam@free.fr>
2018-10-11 23:20:32 +0200

    save

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # FreeFW
 
 A Free simple PHP Framework
+
+## Composer
+
+Just read composer.json...
+
+## Standards
+
+* PHP  : PSR2, PSR3, PSR4, PSR7 & PSR15
+* Other : jsonApi
--- src/FreeFW/Core/ApiController.php
+++ src/FreeFW/Core/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends \FreeFW\Core\Controller
      */
     public function createOne(\Psr\Http\Message\ServerRequestInterface $p_request)
     {
-        $this->logger->debug('FreeFW.ApiController.addOne.start');
+        $this->logger->debug('FreeFW.ApiController.createOne.start');
         $apiParams = $p_request->getAttribute('api_params', false);
         //
         if ($apiParams->hasData()) {
@@ -27,8 +27,56 @@ class ApiController extends \FreeFW\Core\Controller
             if ($data->isValid()) {
                 $data->create();
             }
-            $this->logger->debug('FreeFW.ApiController.addOne.end');
+            $this->logger->debug('FreeFW.ApiController.createOne.end');
+            return $this->createResponse(201, $data);
+        } else {
+            return $this->createResponse(409);
+        }
+    }
+
+    /**
+     * Update single element
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $p_request
+     */
+    public function updateOne(\Psr\Http\Message\ServerRequestInterface $p_request)
+    {
+        $this->logger->debug('FreeFW.ApiController.updateOne.start');
+        $apiParams = $p_request->getAttribute('api_params', false);
+        //
+        if ($apiParams->hasData()) {
+            /**
+             * @var \FreeFW\Core\StorageModel $data
+             */
+            $data = $apiParams->getData();
+            if ($data->isValid()) {
+                $data->update();
+            }
+            $this->logger->debug('FreeFW.ApiController.updateOne.end');
             return $this->createResponse(200, $data);
+        } else {
+            return $this->createResponse(409);
+        }
+    }
+
+    /**
+     * Remove single element
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $p_request
+     */
+    public function removeOne(\Psr\Http\Message\ServerRequestInterface $p_request)
+    {
+        $this->logger->debug('FreeFW.ApiController.removeOne.start');
+        $apiParams = $p_request->getAttribute('api_params', false);
+        //
+        if ($apiParams->hasData()) {
+            /**
+             * @var \FreeFW\Core\StorageModel $data
+             */
+            $data = $apiParams->getData();
+            $data->remove();
+            $this->logger->debug('FreeFW.ApiController.removeOne.end');
+            return $this->createResponse(204);
         } else {
             return $this->createResponse(409);
         }
