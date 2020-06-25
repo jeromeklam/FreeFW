5a08cc0a2a7e890e886d93721d80d43c5e037f0c
ericme <ericme@host.docker.internal>
2020-06-25 12:42:01 +0200

    NVT 283769 : 2 nouvelles méthodes (isCreation, isModification).

--- src/FreeFW/Behaviour/ValidatorTrait.php
+++ src/FreeFW/Behaviour/ValidatorTrait.php
@@ -85,6 +85,30 @@ trait ValidatorTrait
     }
 
     /**
+     * Check if model in creation
+     *
+     * @return boolean
+     * @desc true si la Pk est à zéro. Rappel, la Pk est obligatoire et son type est un entier
+     */
+    public function isCreation()
+    {
+        $getter = $this->getPkGetter();
+        $value = $this->$getter(); // si le get ou la PK n'existent pas, erreur 500.
+        return ($value==null || $value===(int)0); // true si null ou 0
+    }
+
+    /**
+     * Check if model in modification
+     *
+     * @return boolean
+     * @desc true si la Pk n'est pas à zéro. Rappel, la Pk est obligatoire et son type est un entier
+     */
+    public function isModification()
+    {
+        return !$this->isCreation();
+    }
+
+    /**
      * Errors
      *
      * @return bool
