ad88b04440416fb87fea50e00e90a40affbf868a
jeromeklam <jeromeklam@free.fr>
2021-07-18 21:05:07 +0200

    fix(inbox): no-history

--- src/FreeFW/Model/Inbox.php
+++ src/FreeFW/Model/Inbox.php
@@ -15,4 +15,10 @@ class Inbox extends \FreeFW\Model\Base\Inbox
      * Comportements
      */
     use \FreeSSO\Model\Behaviour\User;
+
+    /**
+     * Prevent from saving history
+     * @var bool
+     */
+    protected $no_history = true;
 }
